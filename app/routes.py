75dfd978d353f65cd5ced844fd802c7f905f6f7d
Sabra el hajjaji <selhajja@my.centennialcollege.ca>
2025-04-04 18:56:15 -0400

    Add full ML pipeline and update API to support raw feature input

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,12 @@
 # -*- coding: utf-8 -*-
 """
+Created on Fri Apr  4 11:15:29 2025
+
+@author: m
+"""
+
+# -*- coding: utf-8 -*-
+"""
 Created on Sun Mar 16 14:07:39 2025
 
 @author: sabra
@@ -9,9 +16,13 @@ Created on Sun Mar 16 14:07:39 2025
 from flask import request, jsonify
 import pickle
 import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
 
 # Load the model
-model = pickle.load(open('app/model/model.pkl', 'rb'))
+model = pickle.load(open('app/model/full_pipeline.pkl', 'rb'))
 
 
 def configure_routes(app):
@@ -19,13 +30,24 @@ def configure_routes(app):
     @app.route('/predict', methods=['POST'])
     def predict():
         try:
-            
-            data = request.get_json(force=True)
-            features = [list(data.values())]
-            prediction = model.predict(features)
-            # Convert NumPy int64 to Python int
-            prediction_value = int(prediction[0])
-            return jsonify({'prediction': prediction_value})
+                        
+              # Get JSON data from Postman
+             data = request.get_json()
+             
+             # Convert to DataFrame (ensure keys match your features)
+             input_data = pd.DataFrame([data])
+             
+                       
+             # Predict
+             prediction = model.predict(input_data)
+             proba = model.predict_proba(input_data)[:, 1]  # Probability of "Fatal"
+             
+             return jsonify({
+                 "prediction": int(prediction[0]),
+                 "probability": float(proba[0]),
+                 "message": "Fatal" if prediction[0] == 1 else "Non-Fatal"
+             })
+
 
         except Exception as e:
             return jsonify({'error': str(e)}), 400
